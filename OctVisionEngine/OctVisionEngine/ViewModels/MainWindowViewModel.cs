2375bbbf65c74ad18a2f6820e01e2adfb00ea9f0
Lida <lida.zhu@santec.com>
2025-06-26 15:33:40 +0900

    [Update]: testing code is temporally integrated into avalonia template.

--- OctVisionEngine/OctVisionEngine/ViewModels/MainWindowViewModel.cs
+++ OctVisionEngine/OctVisionEngine/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,46 @@
-﻿namespace OctVisionEngine.ViewModels;
+﻿using System;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Threading.Tasks;
 
-public partial class MainWindowViewModel : ViewModelBase
+namespace OctVisionEngine.ViewModels
 {
-    public string Greeting { get; } = "Text display test #1";
+    public partial class MainWindowViewModel : ObservableObject
+    {
+        private double _input;
+        private double _result; 
+        
+        [ObservableProperty] 
+        private string greeting = "welcome!";
+        
+        public MainWindowViewModel()
+        {
+            // ViewModel initialization logic.
+        }
+
+        [RelayCommand]
+        private async Task RuntestAsync()
+        {
+            // Code here will be executed when the buttom being pressed. 
+            GetInput();
+            CalToOutput();
+        }
+
+        public void GetInput()
+        {
+            Console.WriteLine("Enter your input: ");
+            _input = Convert.ToDouble(Console.ReadLine());
+        }
+
+        private double Calculate()
+        {
+            _result = --_input;
+            return _result;
+        }
+
+        public void CalToOutput()
+        {
+            Console.WriteLine("Your input is: {0}", Calculate());
+        }
+    }
 }
---
+++ OctVisionEngine/OctVisionEngine/Views/MusisStoreWindow.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace OctVisionEngine.Views;
+
+public partial class MusisStoreWindow : Window
+{
+    public MusisStoreWindow()
+    {
+        InitializeComponent();
+    }
+}
