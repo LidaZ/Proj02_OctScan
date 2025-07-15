7700cdab58de7e9d2604b5d62ee2df522a575895
Lida <lida.zhu@santec.com>
2025-07-15 11:12:45 +0900

    [Test]: Unit test module added.

---
+++ OctVisionEngine/OctVisionEngine/Models/CodeTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+// using NUnit.Framework;
+
+namespace OctVisionEngine.Models;
+
+public class CodeTest
+{
+    public static int Add(int x, int y) => x + y;
+    public static int Subtract(int x, int y) => x - y;
+    // private string aFriend = "Bill";
+    // Console.WriteLine(aFriend);
+}
---
+++ OctVisionEngine/UnitTestTool/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using OctVisionEngine.Models; 
+
+namespace UnitTestTool;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+        Console.WriteLine("=== 测试开始 ===");
+    }
+
+    [Test]
+    public void TestAdd()
+    {
+        int x = 5;
+        int y = 3;
+        int expeted = 8;
+        
+        int result = CodeTest.Add(x, y);
+        Assert.That(result, Is.EqualTo(expeted));
+        Console.WriteLine("Add 测试通过");
+    }
+    
+    [Test]
+    public void TestSubtract()
+    {
+        int x = 5;
+        int y = 3;
+        int expeted = 2;
+        
+        int result = CodeTest.Subtract(x, y);
+        Assert.That(result, Is.EqualTo(expeted));
+        Console.WriteLine("Subtract 测试通过");
+    }
+}
